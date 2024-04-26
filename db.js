7f0554c8f9cc12edccd6d40bd79925e251acbece
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-26 12:44:41 +0200

    ⚙️

--- db.js
+++ db.js
@@ -1,15 +1,14 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
 
-const pool = mysql.createPool({ // Création d'un pool de connexion à la base de données 
+dotenv.config(); // Load environment variables from .env file
 
+const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-
 });
 
-
 export default pool;
-
