faf29c808607c443a45361cddebe69efbf576724
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-26 12:41:38 +0200

    Update db.js

--- db.js
+++ db.js
@@ -2,12 +2,12 @@ import mysql from "mysql2";
 
 const pool = mysql.createPool({ // Création d'un pool de connexion à la base de données 
 
-  host: process.env.HOST,
-  port: process.env.PORT ,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  
+
 });
 
 
