cab87e9b5238c7d47093e27d3983e4146e9940f4
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-28 18:10:19 +0200

    clear index

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import ip from "ip";
-import router from './router.js';
-import { query } from "./queries.js";
-
-
+import router from "./router.js";
 
 const app = express(); // Création de l'application express
 const port = process.env.PORT || 3000; // Définition du port d'écoute du serveur
@@ -15,18 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(router);
-
-// test query to the database
-
-app.get('/', async (req, res) => {
-  try {
-    const result = await query('SELECT * FROM users');
-    res.status(200).json(result);
-    } catch (error) {  
-    res.status(400).json({ error });
-    }
-    });
-
-
 // Start the server
-app.listen(port, () => console.log(`Server is running on http://${ip.address()}:${port}`));
+app.listen(port, () =>
+  console.log(`Server is running on http://${ip.address()}:${port}`),
+);
