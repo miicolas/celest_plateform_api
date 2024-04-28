719ad4fba64f49d5fb69bd22a686db4390f1fa65
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-28 18:10:00 +0200

    add event route

--- router.js
+++ router.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 import authRoute from "./routes/authRoute.js";
+import eventRoute from "./routes/eventRoute.js";
 
 const router = express.Router();
 
 // Routes publiques
 router.use("/auth", authRoute);
-
+router.use("/event", eventRoute);
 
 export default router;
