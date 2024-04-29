2bb8fa370ff984c553d68bb6a7391e9f28bf0722
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-04-29 18:52:03 +0200

    ⚙️

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import ip from "ip";
 import router from "./router.js";
+import cors from "cors";
 
 const app = express(); // Création de l'application express
 const port = process.env.PORT || 3000; // Définition du port d'écoute du serveur
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
