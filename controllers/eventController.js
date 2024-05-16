02f78d8291519664feaf1262d8d6ffe0815a534a
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-05-16 21:02:06 +0200

    🎉 event

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -28,4 +28,32 @@ const getEvents = async (req, res) => {
   }
 };
 
-export { createEvent, getEvents };
+const getEvent = async (req, res) => {
+  try {
+    const { id_name } = req.params;
+
+    const event = await query("SELECT * FROM events WHERE id_name = ?", [
+      id_name,
+    ]);
+
+    res.status(200).json({ event: event });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+};
+
+const participateEvent = async (req, res) => {
+  try {
+    const { id_name } = req.params;
+    const user = req.user.id;
+
+    await query("INSERT INTO participants (id_event, id_user) VALUES (?, ?)", [
+      id_name,
+      user,
+    ]);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+};
+
+export { createEvent, getEvents, getEvent, participateEvent };
--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -2,9 +2,16 @@
 import express from "express";
 const router = express.Router();
 
-import { createEvent, getEvents } from "../controllers/eventController.js";
+import {
+  createEvent,
+  getEvents,
+  getEvent,
+  participateEvent,
+} from "../controllers/eventController.js";
 
 router.post("/createevent", createEvent);
 router.get("/getevents", getEvents);
+router.get("/:id_name", getEvent);
+router.post("/:id_name/participate", participateEvent);
 
 export default router;
